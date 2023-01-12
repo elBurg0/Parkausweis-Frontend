551dc68dabaa413c5e7b11eeb46abc341f87a98e
Uzeyir Mammadov <uzeyirmammad@gmail.com>
2023-01-12 22:22:18 +0100

    Web3 connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Web3 from "web3";
+import abi from "./abi.json";
 
-function App() {
+const MyComponent = () => {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [input3, setInput3] = useState("");
+  const web3 = new Web3(window.ethereum);
+  const contract = new web3.eth.Contract(
+    abi,
+    "0x90404922b94280e6579c02542eef80b2c9164486"
+  );
+
+  console.log(contract);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    contract.methods.getParkingPass("ABC");
+  };
+
+  const handleClaimPass = async () => {
+    const numbersplate = "XYZ123";
+    const place = "Berlin";
+
+    const accounts = await web3.eth.getAccounts();
+    contract.methods
+      .claimParkingPass(numbersplate, place)
+      .send({ from: accounts[0] })
+      .then(console.log)
+      .catch(console.error);
+    console.log("Hello");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        padding: "20px",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div>
+        <h3>Parking Verification</h3>
+      </div>
+      <div
+        style={{ display: "flex", flexDirection: "column", padding: "20px" }}
+      >
+        <form>
+          <input value={input3} onChange={(e) => setInput3(e.target.value)} />
+          <button type="button" onSubmit={handleClaimPass}>
+            Check
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default MyComponent;
