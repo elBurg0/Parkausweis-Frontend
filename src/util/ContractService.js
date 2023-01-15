862ba2a252269bfc1fc0caa4d081b86695b3946e
Leon - Mac <burger.leon@web.de>
2023-01-16 00:19:42 +0100

    Fixed wallet switching

--- src/util/ContractService.js
+++ src/util/ContractService.js
@@ -1,24 +1,13 @@
 import Web3 from "web3";
+import init_wallet from "./WalletService";
 const abi = require("./abi.json");
 
-let account = '';
-
 const web3 = new Web3(window.ethereum);
-web3.eth.requestAccounts()
-.then((accounts) => {
-    if (accounts.length > 0) {
-        account = accounts[0];
-        console.log('Connected Wallet: ', account)
-    } else {console.log('Please connect MetaMask account')}
-})
-.catch((error) => {
-    console.log('Error while connecting wallet: ', error)
-});
-
 const contractAddress="0x6fdFc92F8E38e0d255C56510D3a8D5A940Bf28AE";
 const contract = new web3.eth.Contract(abi, contractAddress);
 
 export async function claimParkingPass(numbersplate, place){
+    const account = await init_wallet();
     const req = await contract.methods
         .claimParkingPass(numbersplate, place)
         .send({ from: account })
@@ -26,6 +15,7 @@ export async function claimParkingPass(numbersplate, place){
 }
 
 export async function claimVisitorPass(numbersplate){
+    const account = await init_wallet();
     const req = await contract.methods
         .claimVisitorPass(numbersplate, Date.now())
         .send({ from: account })
@@ -33,23 +23,25 @@ export async function claimVisitorPass(numbersplate){
 }
 
 export async function renewParkingPass(){
+    const account = await init_wallet();
     const req = await contract.methods
         .renewParkingPass()
         .send({ from: account })
     return req;
 }
 
-export function confirmParkingPass(numbersplate){
-    const req = contract.methods
+export async function confirmParkingPass(numbersplate){
+    const account = await init_wallet();
+    const req = await contract.methods
         .confirmParkingPass(numbersplate, Date.now())
         .send({ from: account })
     return req;
 }
 
-export function verifyParkingPass(numbersplate){
-    const req = contract.methods
+export async function verifyParkingPass(numbersplate){
+    const account = await init_wallet();
+    const req = await contract.methods
         .verifyParkingPass(numbersplate)
         .call({ from: account })
-    console.log(req);
     return req;
 }
