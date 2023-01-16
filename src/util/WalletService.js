2322e5372b82783508da78e09e31fefbe03bde54
Leon - Mac <burger.leon@web.de>
2023-01-16 16:09:21 +0100

    Add form address function and update format

--- src/util/WalletService.js
+++ src/util/WalletService.js
@@ -1,25 +1,34 @@
 import Web3 from "web3";
 
-export default async function init_wallet(){
-    
-    if (window.ethereum){
-        // For initial startup
-        try {
-            const web3 = new Web3(window.ethereum);
-            const accounts = await web3.eth.requestAccounts();
-            const account = accounts[0];
-            return account
-        } catch (error) {
-            return 'Please connect wallet'
-        }
-    } else {
-        return 'Please install MetaMask'
+export default async function init_wallet() {
+  if (window.ethereum) {
+    // For initial startup
+    try {
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.requestAccounts();
+      const account = accounts[0];
+      return account;
+    } catch (error) {
+      return "";
     }
-};
+  } else {
+    return "no_metamask";
+  }
+}
 
-export async function get_walletbalance(walletAddress){
+export async function get_walletbalance(walletAddress) {
+  if (window.ethereum) {
     const web3 = new Web3(window.ethereum);
     var balance = await web3.eth.getBalance(walletAddress); //Will give value in.
-    balance = '0.' + balance
+    balance = "0." + balance;
     return balance;
+  } else {
+    return 0;
+  }
+}
+
+export function form_address(address) {
+  var strFirst = address.substring(0, 4);
+  var strLast = address.substring(address.length - 3);
+  return strFirst + "..." + strLast;
 }
