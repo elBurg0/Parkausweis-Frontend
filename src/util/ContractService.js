e4db151ed7d57c5625bb38e17b0df7af6a222503
Leon - Win <burger.leon@web.de>
2023-01-17 16:53:57 +0100

    Update contract

--- src/util/ContractService.js
+++ src/util/ContractService.js
@@ -3,7 +3,7 @@ import init_wallet from "./WalletService";
 const abi = require("./abi2.json");
 
 const web3 = new Web3(window.ethereum);
-const contractAddress = "0xA5c387419f27329071E4366e931a3903bC2d302A";
+const contractAddress = "0x1dc2Bd7E251778Ff9F3193bE7cb827220fe6867b";
 const contract = new web3.eth.Contract(abi, contractAddress);
 
 export async function claimParkingPass(numbersplate, place) {
@@ -41,5 +41,21 @@ export async function verifyParkingPass(numbersplate) {
   const req = await contract.methods
     .verifyParkingPass(numbersplate.toLowerCase())
     .call({ from: account });
+  return req;
+}
+
+export async function addConfirmerAddress(address) {
+  const account = await init_wallet();
+  const req = await contract.methods
+    .addConfirmer(address.toLowerCase())
+    .send({ from: account });
+  return req;
+}
+
+export async function addWorkerAddress(address) {
+  const account = await init_wallet();
+  const req = await contract.methods
+    .addWorker(address.toLowerCase())
+    .send({ from: account });
   return req;
 }
